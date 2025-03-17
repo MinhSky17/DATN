4b3ad37865d62a47f95f57b82e59980b1e9c1c64
Minh Nguyen <nguyenthiminh.sky.17@gmail.com>
2025-03-17 08:27:24 +0700

    Tuan1

---
+++ Model1/model1-backend/src/main/java/com/example/model1_backend/Model1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.model1_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Model1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Model1BackendApplication.class, args);
+	}
+
+}
---
+++ Model1/model1-backend/src/test/java/com/example/model1_backend/Model1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.model1_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Model1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
